68e9cc5c2d400789b516a634ef1ffc8641761665
Leonardo Maron <leo22.maron@hotmail.com>
2022-11-17 22:41:02 -0300

    02 - Obtendo os grupos

--- 02-igniteteams/src/storage/group/groupCreate.ts
+++ 02-igniteteams/src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { groupsGetAll } from './groupsGetAll';
+
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 
 export async function groupCreate(newGroup: string) {
   try {
-    await AsyncStorage.setItem(GROUP_COLLECTION, newGroup);
+    const storedGroups = await groupsGetAll();
+
+    await AsyncStorage.setItem(
+      GROUP_COLLECTION, 
+      JSON.stringify([...storedGroups, newGroup])
+    );
   } catch(error) {
     throw error;
   }
