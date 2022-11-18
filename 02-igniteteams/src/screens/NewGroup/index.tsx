9992f5bbe5a8bea32a91656253ed2e47a00d7d06
Leonardo Maron <leo22.maron@hotmail.com>
2022-11-18 09:37:15 -0300

    02 - Tratando exceções

--- 02-igniteteams/src/screens/NewGroup/index.tsx
+++ 02-igniteteams/src/screens/NewGroup/index.tsx
@@ -1,5 +1,9 @@
+import { Alert } from 'react-native';
+
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
+import { AppError } from '@utils/AppError';
 
 import { groupCreate } from '@storage/group/groupCreate';
 
@@ -17,10 +21,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        throw new AppError('Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
