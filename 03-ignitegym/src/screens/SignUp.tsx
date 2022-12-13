d010ff3dcf6f9f73bbfc705c96e01a0354b15862
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-13 12:02:13 -0300

    03 - Aplicando navegação de autenticação

--- 03-ignitegym/src/screens/SignUp.tsx
+++ 03-ignitegym/src/screens/SignUp.tsx
@@ -1,5 +1,7 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
+import { useNavigation } from '@react-navigation/native';
+
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
@@ -7,6 +9,12 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <ScrollView 
       contentContainerStyle={{ flexGrow: 1 }}
@@ -54,7 +62,8 @@ export function SignUp() {
         <Button 
           title='Voltar para o login' 
           variant='outline' 
-          mt={24} 
+          mt={24}
+          onPress={handleGoBack}
         />
       </VStack>
     </ScrollView>
