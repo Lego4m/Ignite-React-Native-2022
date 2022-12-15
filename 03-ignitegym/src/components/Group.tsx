0b6741f7761072c55c0b3caed91d7078d49330b6
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-15 07:37:12 -0300

    03 - Exibindo o grupo selecionado

--- 03-ignitegym/src/components/Group.tsx
+++ 03-ignitegym/src/components/Group.tsx
@@ -2,9 +2,10 @@ import { Text, Pressable, IPressableProps } from 'native-base';
 
 type GroupProps = IPressableProps & {
   name: string;
+  isActive: boolean;
 }
 
-export function Group({ name, ...rest }: GroupProps) {
+export function Group({ name, isActive, ...rest }: GroupProps) {
   return (
     <Pressable
       mr={3}
@@ -15,6 +16,7 @@ export function Group({ name, ...rest }: GroupProps) {
       justifyContent='center'
       alignItems='center'
       overflow='hidden'
+      isPressed={isActive}
       _pressed={{
         borderColor: 'green.500',
         borderWidth: 1,
@@ -22,7 +24,7 @@ export function Group({ name, ...rest }: GroupProps) {
       {...rest}
     >
       <Text
-        color='gray.200'
+        color={isActive ? 'green.500' : 'gray.200'}
         textTransform='uppercase'
         fontSize='xs'
         fontWeight='bold'
--- 03-ignitegym/src/screens/Home.tsx
+++ 03-ignitegym/src/screens/Home.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
+
 import { HStack, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name='costas' />
-        <Group name='ombro' />
+        <Group 
+          name='costas' 
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+
+        <Group 
+          name='ombro' 
+          isActive={groupSelected === 'ombro'} 
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
     </VStack>
   )
