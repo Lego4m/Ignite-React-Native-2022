77aeef26e295fa5275afa86cf305d5059ea5bfc6
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-19 19:43:44 -0300

    03 - Imagem do Exercício

--- 03-ignitegym/src/screens/Exercise.tsx
+++ 03-ignitegym/src/screens/Exercise.tsx
@@ -4,7 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 
 import { Feather } from '@expo/vector-icons';
 
-import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack, Image } from 'native-base';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
@@ -37,6 +37,18 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image 
+          w='full'
+          h={80}
+          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          alt='Nome do exercício'
+          mb={3}
+          resizeMode='cover'
+          rounded='lg'
+        />
       </VStack>
     </VStack>
   )
