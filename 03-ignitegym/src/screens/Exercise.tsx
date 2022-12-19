bb9df09a7f808f2129b0d408d37b05650d5ca523
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-19 19:38:21 -0300

    03 - Finalizando o Header do Exercício

--- 03-ignitegym/src/screens/Exercise.tsx
+++ 03-ignitegym/src/screens/Exercise.tsx
@@ -4,9 +4,11 @@ import { useNavigation } from '@react-navigation/native';
 
 import { Feather } from '@expo/vector-icons';
 
-import { Icon, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
+import BodySvg from '@assets/body.svg';
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -21,6 +23,20 @@ export function Exercise() {
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={Feather} name='arrow-left' color='green.500' size={6} />
         </TouchableOpacity>
+
+        <HStack justifyContent='space-between' mt={4} mb={8} alignItems='center'>
+          <Heading color='gray.100' fontSize='lg' flexShrink={1}>
+            Puxada frtontal
+          </Heading>
+
+          <HStack alignItems='center'>
+            <BodySvg />
+
+            <Text color='gray.200' ml={1} textTransform='capitalize'>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   )
