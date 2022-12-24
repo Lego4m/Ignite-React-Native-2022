16d3d73eb6f3d65cc88f6174eee8f6ef13a14f37
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-24 09:14:55 -0300

    03 - Ajustando Heading

--- 03-ignitegym/src/components/ExerciseCard.tsx
+++ 03-ignitegym/src/components/ExerciseCard.tsx
@@ -23,7 +23,7 @@ export function ExerciseCard({ ...rest }: ExerciseCardProps) {
         />
 
         <VStack flex={1}>
-          <Heading fontSize='lg' color ='white'>
+          <Heading fontSize='lg' color ='white' fontFamily='heading'>
             Remada unilateral
           </Heading>
 
