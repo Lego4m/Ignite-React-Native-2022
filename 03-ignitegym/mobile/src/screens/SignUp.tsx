0623593d44411dd7378243cf2f8b8dbe07173b1c
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-29 08:51:13 -0300

    03 - Criando conta e logando usuario

--- 03-ignitegym/mobile/src/screens/SignUp.tsx
+++ 03-ignitegym/mobile/src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 
 import { useNavigation } from '@react-navigation/native';
@@ -16,6 +17,8 @@ import { api } from '@services/api';
 
 import { AppError } from '@utils/AppError';
 
+import { useAuth } from '@hooks/useAuth';
+
 type FormDataProps = {
   name: string;
   email: string;
@@ -31,7 +34,10 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const toast = useToast();
+  const { signIn } = useAuth();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -45,9 +51,14 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password });
-      console.log(response);
+      setIsLoading(true);
+
+      await api.post('/users', { name, email, password });
+      await signIn(email, password);
+
     } catch (error) {
+      setIsLoading(false);
+
       const isAppError = error instanceof AppError;
       const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
 
@@ -144,7 +155,11 @@ export function SignUp() {
             )}
           />
 
-          <Button title='Criar e acessar' onPress={handleSubmit(handleSignUp)} />
+          <Button 
+            title='Criar e acessar' 
+            onPress={handleSubmit(handleSignUp)} 
+            isLoading={isLoading}
+          />
         </Center>
          
         <Button 
