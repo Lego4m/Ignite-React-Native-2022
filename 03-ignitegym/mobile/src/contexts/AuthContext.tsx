541dcca002db829f3ec4ed0f76d9f715d71f66c4
Leonardo Maron <leo22.maron@hotmail.com>
2022-12-29 08:45:23 -0300

    03 - Criando o método signOut

--- 03-ignitegym/mobile/src/contexts/AuthContext.tsx
+++ 03-ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from  'react';
 
-import { storageUserSave, storageUserGet } from '@storage/storageUser';
+import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
 
 import { api } from '@services/api';
 
@@ -9,6 +9,7 @@ import { UserDTO } from '@dtos/UserDTO';
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
   isLoadingUserStorageData: boolean;
 }
 
@@ -32,6 +33,21 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       }
     } catch (error) {
       throw (error);
+    }
+  }
+
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      setUser({} as UserDTO);
+      await storageUserRemove();
+
+    } catch (error) {
+      throw error;
+
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -59,7 +75,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       value={{ 
         user,
         signIn,
-        isLoadingUserStorageData
+        signOut,
+        isLoadingUserStorageData,
       }}
     >
       {children}
