a7dd376f7f34a67963591dcdec6c972258654518
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-03 09:43:30 -0300

    03 - Carregando histórico

--- 03-ignitegym/mobile/src/screens/History.tsx
+++ 03-ignitegym/mobile/src/screens/History.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 
-import { Heading, VStack, SectionList, Text } from 'native-base';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
+import { api } from '@services/api';
+
+import { AppError } from '@utils/AppError';
+
 export function History() {
+  const [isLoading, setIsLoading] = useState(true);
   const [exercises, setExercises] = useState([
     {
       title: '26.08.22',
@@ -16,6 +22,33 @@ export function History() {
       data: ['Puxada frontal']
     },
   ]);
+
+  const toast = useToast();
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+
+      const { data } = await api.get('/history');
+
+      console.log(data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível carregar o histórico.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchHistory();
+  }, []));
 
   return (
     <VStack flex={1}>
