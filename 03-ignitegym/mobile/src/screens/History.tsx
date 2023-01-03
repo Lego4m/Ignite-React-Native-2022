3977bdb7e175eb9fbce1f58e06ae7ae6f72c7684
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-03 09:54:06 -0300

    03 - Listando o histórico

--- 03-ignitegym/mobile/src/screens/History.tsx
+++ 03-ignitegym/mobile/src/screens/History.tsx
@@ -7,21 +7,13 @@ import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 import { api } from '@services/api';
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO';
 
 import { AppError } from '@utils/AppError';
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
-  const [exercises, setExercises] = useState([
-    {
-      title: '26.08.22',
-      data: ['Puxada frontal', 'Remada unilateral']
-    },
-    {
-      title: '27.08.22',
-      data: ['Puxada frontal']
-    },
-  ]);
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
 
   const toast = useToast();
 
@@ -31,7 +23,7 @@ export function History() {
 
       const { data } = await api.get('/history');
 
-      console.log(data);
+      setExercises(data);
     } catch (error) {
       const isAppError = error instanceof AppError;
       const title = isAppError ? error.message : 'Não foi possível carregar o histórico.';
@@ -56,7 +48,7 @@ export function History() {
 
       <SectionList 
         sections={exercises}
-        keyExtractor={item => item}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <HistoryCard />
         )}
