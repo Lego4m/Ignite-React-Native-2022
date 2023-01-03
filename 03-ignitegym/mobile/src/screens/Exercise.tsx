b65623a68dbccedb03638e86ef370f91d1c8f5f5
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-02 21:48:55 -0300

    03 - Registrando histórico

--- 03-ignitegym/mobile/src/screens/Exercise.tsx
+++ 03-ignitegym/mobile/src/screens/Exercise.tsx
@@ -27,6 +27,8 @@ type RouteParamsProps = {
 }
 
 export function Exercise() {
+  const [sendingRegister, setSendingRegister] = useState(false);
+
   const [isLoading, setIsLoading] = useState(true);
   const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
 
@@ -61,6 +63,33 @@ export function Exercise() {
     }
   }
   
+  async function handleExerciseHistoryRegister() {
+    try {
+      setSendingRegister(true);
+
+      await api.post('/history', { exercise_id: exerciseId });
+
+      toast.show({
+        title: 'Parabéns! Exercício registrado com sucesso.',
+        placement: 'top',
+        bgColor: 'green.700',
+      });
+
+      navigation.navigate('history');
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível registrar o exercício.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setSendingRegister(false);
+    }
+  }
+
   useEffect(() => {
     fetchExerciseDetails();
   }, [exerciseId]);
@@ -124,6 +153,8 @@ export function Exercise() {
 
             <Button 
               title='Marcar como realizado'
+              isLoading={sendingRegister}
+              onPress={handleExerciseHistoryRegister}
             />
           </Box>
         </ScrollView>
