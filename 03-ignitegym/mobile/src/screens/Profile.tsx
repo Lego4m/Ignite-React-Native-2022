ee15231b9b262409241a148863c901ce622cc80c
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-05 10:15:45 -0300

    03 - Atualizando usuário no back-end

--- 03-ignitegym/mobile/src/screens/Profile.tsx
+++ 03-ignitegym/mobile/src/screens/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TouchableOpacity, Alert } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
 
@@ -17,6 +17,9 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 import { useAuth } from '@hooks/useAuth';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 const PHOTO_SIZE = 33;
 
@@ -53,6 +56,7 @@ const profileSchema = yup.object({
 });
 
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/Lego4m.png');
 
@@ -101,7 +105,28 @@ export function Profile() {
   }
 
   async function handleProfileUpdate(data: FormDataProps) {
-    console.log(data);
+    try {
+      setIsUpdating(true);
+
+      await api.put('/users', data);
+
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível atualizar os dados. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsUpdating(false);
+    }
   }
 
   return (
@@ -216,6 +241,7 @@ export function Profile() {
             title='Atualizar'
             mt={4}
             onPress={handleSubmit(handleProfileUpdate)}
+            isLoading={isUpdating}
           />
         </Center>
       </ScrollView>
