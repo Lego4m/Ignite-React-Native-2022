52a843969c3cadba9d1f0e4b34915ea8a55281cc
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-08 12:41:18 -0300

    03 - Incluindo gerenciador de Token na API

--- 03-ignitegym/mobile/src/services/api.ts
+++ 03-ignitegym/mobile/src/services/api.ts
@@ -1,17 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 import { AppError } from '@utils/AppError';
 
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
+
 const api = axios.create({
   baseURL: 'http://192.168.0.106:3333'
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use((response) => response, (error) => {
-  if (error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message));
+api.registerInterceptTokenManager = signOut => {
+  const interceptTokenManager = api
+    .interceptors.response.use((response) => response, (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message));
+      }
+    
+      return Promise.reject(error);
+    });
+  
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
   }
-
-  return Promise.reject(error);
-});
+}
 
 export { api };
