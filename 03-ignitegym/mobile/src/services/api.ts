0feaf7608cf4df44626e98c3da393f9e28d89646
Leonardo Maron <leo22.maron@hotmail.com>
2023-01-09 10:09:26 -0300

    03 - Colocando requisições na fila

--- 03-ignitegym/mobile/src/services/api.ts
+++ 03-ignitegym/mobile/src/services/api.ts
@@ -6,6 +6,11 @@ import { storageAuthTokenGet } from '@storage/storageAuthToken';
 
 type SignOut = () => void;
 
+type PromiseType = {
+  resolve: (value?: unknown) => void;
+  reject: (reason?: unknown) => void;
+}
+
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
 };
@@ -13,6 +18,9 @@ type APIInstanceProps = AxiosInstance & {
 const api = axios.create({
   baseURL: 'http://192.168.0.106:3333'
 }) as APIInstanceProps;
+
+let isRefreshing = false;
+const failedQueue: PromiseType[] = [];
 
 api.registerInterceptTokenManager = signOut => {
   const interceptTokenManager = api
@@ -27,6 +35,23 @@ api.registerInterceptTokenManager = signOut => {
             signOut();
             return Promise.reject(requestError);
           }
+
+          const originalRequest = requestError.config;
+          
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({ resolve, reject });
+            })
+              .then((token) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axios(originalRequest);
+              })
+              .catch((error) => {
+                throw error;
+              });
+          }
+
+          isRefreshing = true;
         }
 
         signOut();
